8ea1f324e6860c1f7ebd7a7ef31ddf407e88ab4d
Petter Nilsson <pettni@caltech.edu>
2018-01-30 13:26:10 -0800

    parameter updates

--- rss18/ros_simulate.py
+++ rss18/ros_simulate.py
@@ -1,42 +1,121 @@
 import numpy as np
 import rospy
-from best_waypoints.srv import *
+from env_exploration.srv import *
 
 import matplotlib.pyplot as plt
 
 rospy.wait_for_service('copter_waypoints')
 
-handle = rospy.ServiceProxy('copter_waypoints', Waypoint)
+cop_waypts = rospy.ServiceProxy('copter_waypoints', Waypoint)
+rov_waypts = rospy.ServiceProxy('rover_waypoints', Waypoint)
 
-x_copter = np.array([-0.5, -4.5, 2]).reshape((1,3))
+# reset all
+rov_req = WaypointRequest()
+cop_req = WaypointRequest()
+rov_req.reset_map = True
+cop_req.reset_map = True
+rov_waypts(rov_req)
+cop_waypts(cop_req)
 
-for i in range(200):
+# initialize variables
+rover_finished = False
+copter_finished = False
+value = 1.
+map_belief = None
+
+x_copter = np.array([-4.5, -1.5, 2]).reshape((1,3))
+x_rover = np.array([-4.5, -0.5]).reshape((1,2))
+
+# rover
+while not rover_finished and value > 0.85:
+
+  x_curr = x_rover[-1, :].flatten()
+  rov_req.current.x = x_curr[0]
+  rov_req.current.y = x_curr[1]
+
+  rov_req.reset_map = False
+
+  rov_res = rov_waypts(rov_req)
+  
+  x_next = np.zeros(2)
+  x_next[0] = rov_res.target.x
+  x_next[1] = rov_res.target.y
+
+  value = rov_res.value
+  rover_finished = rov_res.finished
+  map_belief = rov_res.map_belief
+
+  if np.linalg.norm(x_next - x_curr) > 0:
+      x_del = 0.1*(x_next - x_curr)/np.linalg.norm(x_next - x_curr)
+  else:
+      x_del = 0
+  x_rover = np.vstack([x_rover, x_curr + x_del])
+
+print 'before exploration: value {}, map belief: {}'.format(value, map_belief)
+
+# copter
+while not copter_finished:
 
   x_curr = x_copter[-1, :].flatten()
 
-  req = WaypointRequest()
-  req.current.x = x_curr[0]
-  req.current.y = x_curr[1]
-  req.current.z = x_curr[2]
+  cop_req.current.x = x_curr[0]
+  cop_req.current.y = x_curr[1]
+  cop_req.current.z = x_curr[2]
 
-  req.reset_map = False
+  cop_req.reset_map = False
 
-  res = handle(req)
-  
-  print res
-
+  print cop_req
+  cop_res = cop_waypts(cop_req)
+  print cop_res
   x_next = np.zeros(3)
-  x_next[0] = res.target.x
-  x_next[1] = res.target.y
-  x_next[2] = res.target.z
+  x_next[0] = cop_res.target.x
+  x_next[1] = cop_res.target.y
+  x_next[2] = cop_res.target.z
 
-  x_del = 0.1*(x_next - x_curr)/np.linalg.norm(x_next - x_curr)
+  copter_finished = cop_res.finished
+  map_belief = cop_res.map_belief
 
+  if np.linalg.norm(x_next - x_curr) > 0:
+      x_del = 0.1*(x_next - x_curr)/np.linalg.norm(x_next - x_curr)
+  else:
+      x_del = 0
 
   x_copter = np.vstack([x_copter, x_curr + x_del])
+
+print 'after exploration: value {}, map belief: {}'.format(value, map_belief)
+
+# rover again
+value = 1
+while not rover_finished and value > 0.85:
+
+  x_curr = x_rover[-1, :].flatten()
+  rov_req.current.x = x_curr[0]
+  rov_req.current.y = x_curr[1]
+
+  rov_req.reset_map = False
+
+  rov_res = rov_waypts(rov_req)
+  
+  x_next = np.zeros(2)
+  x_next[0] = rov_res.target.x
+  x_next[1] = rov_res.target.y
+
+  value = rov_res.value
+  rover_finished = rov_res.finished
+  map_belief = rov_res.map_belief
+
+  if np.linalg.norm(x_next - x_curr) > 0:
+      x_del = 0.1*(x_next - x_curr)/np.linalg.norm(x_next - x_curr)
+  else:
+      x_del = 0
+  x_rover = np.vstack([x_rover, x_curr + x_del])
+
 
 fig = plt.figure()
 ax = plt.Axes(fig, [0.,0.,1.,1.])
 fig.add_axes(ax)
 
 ax.plot(x_copter[:, 0], x_copter[:, 1], color='blue', linewidth=2)
+ax.plot(x_rover[:, 0], x_rover[:, 1], color='red', linewidth=2)
+
+plt.show()
