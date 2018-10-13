fcd12803f75e4a22d73a4100415e79b4086193cf
Petter Nilsson <pettni@caltech.edu>
2018-10-12 21:23:25 -0700

    ROB interface now works

--- examples/CAST/rob_interface.py
+++ examples/CAST/rob_interface.py
@@ -5,18 +5,22 @@ from cyberpod_ros.srv import params, paramsRequest, paramsResponse
 class RobCMD:
 
   def __init__(self):
-    self.pub = rospy.Publisher('/cyberpod_cmd_throttled', command, queue_size=10)
-    self.srv = rospy.ServiceProxy('cyberpod_ros/params', params)
+    self.pub = rospy.Publisher('cyberpod_cmd_throttled', command, queue_size=10)
+    self.srv = rospy.ServiceProxy('cyberpod_params', params)
 
   def goto(self, x, y):
-    msg = command([x, y])
+    msg = command([x, y, 1.])
+    self.pub.publish(msg)
+
+  def hold(self):
+    msg = command([0., 0., 0.])
     self.pub.publish(msg)
 
   def set_run(self):
-    self.service(paramsRequest(5, 2, [0.]*20))
+    self.srv(paramsRequest(5, 2, [0.]*20))
 
   def set_idle(self):
-    self.service(paramsRequest(5, 1, [0.]*20))
+    self.srv(paramsRequest(5, 1, [0.]*20))
 
   def set_failure(self):
-    self.service(paramsRequest(5, 0, [0.]*20))
+    self.srv(paramsRequest(5, 0, [0.]*20))
